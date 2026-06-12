fb911cbaf134b99cc1929dd5a83ca43021392bf6
ananyaaaguptaaa <ananya000guptaa@gmail.com>
2026-06-12 21:22:59 +0530

    Second Class

--- ElectricityBill.java
+++ ElectricityBill.java
@@ -21,3 +21,22 @@ class Consumer {
         return unitsConsumed;
     }
 }
+class TariffCalculator {
+
+    public double generateBill(Consumer consumer) {
+        int units = consumer.getUnitsConsumed();
+        double billAmount = 0;
+
+        if (units <= 100) {
+            billAmount = units * 1.5;
+        } else if (units <= 200) {
+            billAmount = (100 * 1.5) + ((units - 100) * 2.5);
+        } else if (units <= 300) {
+            billAmount = (100 * 1.5) + (100 * 2.5) + ((units - 200) * 4.0);
+        } else {
+            billAmount = (100 * 1.5) + (100 * 2.5) + (100 * 4.0) + ((units - 300) * 6.0);
+        }
+
+        return billAmount;
+    }
+}
